5818f7598a6256e331b76dc93a88c67586b72bd1
oliverlai55 <oliverlai55@gmail.com>
2015-12-03 15:32:19 -0500

    adding socket io inside main.js

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ var fs = require('fs');
 
 var server = http.createServer(function (request, response){
 	fs.readFile('client.html', 'utf-8', function (error, data){
-		response.end('Hello World');
+		response.writeHead(200,{'Content-type': 'text/html'});
+		response.write(data);
+		response.end();
 	})
 });
 
 server.listen(21000);
 
-console.log("test");
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+//connection is an event in socket , a node event (when someone connects)
+//.socket is a method, inside the socket module
+//when connect, it will bring in the socket
+	socket.on('message_to_server', function(data){
+		io.sockets.emit('message_to_client', {
+			message: data["message"]
+		})
+	});
+})
